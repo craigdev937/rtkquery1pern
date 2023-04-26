b0a234f7abe9b451eb7bae111653fff1394106f6
Craig <indigomx9@gmail.com>
2023-04-26 12:47:17 -0400

    Created the README.md file.  Updated the Add component.

---
+++ README.md
@@ -0,0 +1,5 @@
+# RTK-Query CRUD
+
+Initial setup of the CRUD Project.  Created with React, Webpack, TypeScript, React Router and RTK-Query.
+
+
--- src/pages/Add.tsx
+++ src/pages/Add.tsx
@@ -1,10 +1,85 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { PlayerAPI } from "../global/PlayerAPI";
 
 export const Add = () => {
+    const navigate = useNavigate();
+    const [addPlayer] = PlayerAPI.useAddMutation();
+    const [player, setPlayer] = React.useState({
+        id: "", title: "", first: "", 
+        last: "", age: 0, info: ""
+    });
+
+    const handleChange = 
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPlayer({...player, 
+            [event.target.name]: event.target.value});
+    };
+
+    const handleSubmit = 
+    async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        await addPlayer(player);
+        setPlayer({
+            id: "", title: "", first: "", 
+            last: "", age: 0, info: ""
+        });
+        navigate("/");
+    };
+
     return (
         <React.Fragment>
-            <h1>Add</h1>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis ex culpa quia odio minus molestiae velit dicta amet dolorem explicabo consectetur atque fugiat, officia obcaecati nihil quo, quis eveniet laborum.</p>
+            <form onSubmit={handleSubmit}>
+                <aside>
+                    <input 
+                        type="text" 
+                        name="title"
+                        placeholder="Title"
+                        value={player.title}
+                        onChange={handleChange}
+                    />
+                </aside>
+                <aside>
+                    <input 
+                        type="text" 
+                        name="first"
+                        placeholder="First"
+                        value={player.first}
+                        onChange={handleChange}
+                    />
+                </aside>
+                <aside>
+                    <input 
+                        type="text" 
+                        name="last"
+                        placeholder="Last"
+                        value={player.last}
+                        onChange={handleChange}
+                    />
+                </aside>
+                <aside>
+                    <input 
+                        type="number" 
+                        name="age"
+                        placeholder="Age"
+                        value={player.age}
+                        onChange={handleChange}
+                    />
+                </aside>
+                <aside>
+                    <input 
+                        type="text" 
+                        name="info"
+                        placeholder="Info"
+                        value={player.info}
+                        onChange={handleChange}
+                    />
+                </aside>
+                <button 
+                    type="submit"
+                    >Add Player
+                </button>
+            </form>
         </React.Fragment>
     );
 };
